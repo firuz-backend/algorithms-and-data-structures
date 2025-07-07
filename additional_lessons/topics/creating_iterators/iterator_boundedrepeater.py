88a61a2f1f5906120b39ecbd47e00c20fe51d482
Firuz Dadabaev <firuz.backend@gmail.com>
2025-07-07 10:46:04 +0500

    created iterator boundedRepeater

---
+++ additional_lessons/topics/creating_iterators/iterator_boundedrepeater.py
@@ -0,0 +1,13 @@
+class BoundedRepeater:
+    def __init__(self, obj, times):
+        self.obj = obj
+        self.times = times
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.times == 0:
+            raise StopIteration
+        self.times -= 1
+        return self.obj
