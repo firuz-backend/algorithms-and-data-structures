af3c39f4b6e8fd9544af730a26577eeee5dc4e28
Firuz Dadabaev <firuz.backend@gmail.com>
2025-08-15 15:06:38 +0500

    Saved changes

---
+++ additional_lessons/topics/creating_generators/generator_is_prime.py
@@ -0,0 +1,10 @@
+def is_prime(number: int):
+    if number == 1:
+        return False
+
+    return all(number % i != 0 for i in range(2, int(number ** 0.5) + 1))
+
+
+print(is_prime(7))
+print(is_prime(8))
+print(is_prime(1))
